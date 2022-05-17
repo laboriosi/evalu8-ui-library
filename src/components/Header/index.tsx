ba965fff7fe381b487fba484611f246de2df0418
Marcos Aurélio <marcos.junior@fretebras.com.br>
2022-05-17 19:28:51 -0300

    feat: update header spacing

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import withTheme from '~hocs/withTheme'
 
 const Header = styled.header`
   margin-left: ${({ theme }) => theme.spacing.s8};
+  margin-top: ${({ theme }) => theme.spacing.s4};
 `
 
 export default withTheme(Header)
